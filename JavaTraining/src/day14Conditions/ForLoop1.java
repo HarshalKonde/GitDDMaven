1b78e6ce5bb2cbb4a20cb6c5b0195d2bfd539401
Harshal Konde <harshalkonde2151@gmail.com>
2022-05-10 12:40:37 +0530

    JavaClasses

---
+++ JavaTraining/src/day14Conditions/ForLoop1.java
@@ -0,0 +1,12 @@
+package day14Conditions;
+
+public class ForLoop1 {      
+	public static void main(String[] args) {      
+		
+		System.out.println("Hello...");
+		System.out.println("Hello...");
+		System.out.println("Hello...");
+		System.out.println("Hello...");
+		System.out.println("Hello...");
+	}  
+}  
